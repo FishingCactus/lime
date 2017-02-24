586d995ed1f1863125701df0a846e89f69945540
Pieter Vantorre <pieter.vantorre@fishingcactus.com>
2017-02-24 12:08:45 +0100

    Removed unused variables

--- lime/audio/AudioManager.hx
+++ lime/audio/AudioManager.hx
@@ -12,132 +12,132 @@ import js.Browser;
 
 
 class AudioManager {
-	
-	
+
+
 	public static var context:AudioContext;
-	
-	
+
+
 	public static function init (context:AudioContext = null) {
-		
+
 		if (AudioManager.context == null) {
-			
+
 			if (context == null) {
-				
+
 				#if (js && html5)
-					
+
 					try {
-						
+
 						untyped __js__ ("window.AudioContext = window.AudioContext || window.webkitAudioContext;");
 						AudioManager.context = WEB (cast untyped __js__ ("new AudioContext ()"));
-						
+
 					} catch (e:Dynamic) {
-						
+
 						AudioManager.context = HTML5 (new HTML5AudioContext ());
-						
+
 					}
-					
+
 				#elseif flash
-					
+
 					AudioManager.context = FLASH (new FlashAudioContext ());
-					
+
 				#elseif lime_console
-					
+
 					// TODO
 					AudioManager.context = CUSTOM (null);
-				
+
 				#else
-					
+
 					AudioManager.context = OPENAL (new ALCAudioContext (), new ALAudioContext ());
-					
+
 					var device = ALC.openDevice ();
 					var ctx = ALC.createContext (device);
 					ALC.makeContextCurrent (ctx);
 					ALC.processContext (ctx);
-					
+
 				#end
-				
+
 			} else {
-				
+
 				AudioManager.context = context;
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	public static function resume ():Void {
-		
+
 		if (context != null) {
-			
+
 			switch (context) {
-				
-				case OPENAL (alc, al):
-					
+
+				case OPENAL (alc, _):
+
 					alc.processContext (alc.getCurrentContext ());
-				
+
 				default:
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	public static function shutdown ():Void {
-		
+
 		if (context != null) {
-			
+
 			switch (context) {
-				
-				case OPENAL (alc, al):
-					
+
+				case OPENAL (alc, _):
+
 					var currentContext = alc.getCurrentContext ();
-					
+
 					if (currentContext != null) {
-						
+
 						var device = alc.getContextsDevice (currentContext);
 						alc.makeContextCurrent (null);
 						alc.destroyContext (currentContext);
 						alc.closeDevice (device);
-						
+
 					}
-				
+
 				case WEB (context):
 					#if (js && html5)
 					untyped __js__ ("context.close()");
 					#end
 
 				default:
-				
+
 			}
-			
+
 			context = null;
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	public static function suspend ():Void {
-		
+
 		if (context != null) {
-			
+
 			switch (context) {
-				
-				case OPENAL (alc, al):
-					
+
+				case OPENAL (alc, _):
+
 					alc.suspendContext (alc.getCurrentContext ());
-				
+
 				default:
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 }
