0759ccb38749713b8f1913b9e9f714e721a8fe07
Pieter Vantorre <pieter.vantorre@fishingcactus.com>
2017-08-25 09:58:27 +0200

    Fixed indentation of Float32Array class

--- lime/utils/Float32Array.hx
+++ lime/utils/Float32Array.hx
@@ -51,9 +51,11 @@ package lime.utils;
             #else
                 return @:privateAccess new haxe.io.Bytes( cast new js.html.Uint8Array(this.buffer) );
             #end
-    }
+        }
 
-        function toString() return this != null ? 'Float32Array [byteLength:${this.byteLength}, length:${this.length}]' : null;
+        function toString() {
+            return this != null ? 'Float32Array [byteLength:${this.byteLength}, length:${this.length}]' : null;
+        }
 
     }
 
@@ -62,13 +64,13 @@ package lime.utils;
     import lime.utils.ArrayBuffer;
     import lime.utils.ArrayBufferView;
 
-@:forward()
-@:arrayAccess
-abstract Float32Array(ArrayBufferView) from ArrayBufferView to ArrayBufferView {
+    @:forward()
+    @:arrayAccess
+    abstract Float32Array(ArrayBufferView) from ArrayBufferView to ArrayBufferView {
 
-    public inline static var BYTES_PER_ELEMENT : Int = 4;
+        public inline static var BYTES_PER_ELEMENT : Int = 4;
 
-    public var length (get, never):Int;
+        public var length (get, never):Int;
 
         @:generic
         public inline function new<T>(
@@ -91,12 +93,14 @@ abstract Float32Array(ArrayBufferView) from ArrayBufferView to ArrayBufferView {
             }
         }
 
-//Public API
+        //Public API
 
-    public inline function subarray( begin:Int, end:Null<Int> = null) : Float32Array return this.subarray(begin, end);
+        public inline function subarray( begin:Int, end:Null<Int> = null) : Float32Array {
+            return this.subarray(begin, end);
+        }
 
 
-            //non spec haxe conversions
+        //non spec haxe conversions
         public static function fromBytes( bytes:haxe.io.Bytes, ?byteOffset:Int=0, ?len:Int ) : Float32Array {
             return new Float32Array(bytes, byteOffset, len);
         }
@@ -105,25 +109,27 @@ abstract Float32Array(ArrayBufferView) from ArrayBufferView to ArrayBufferView {
             return this.buffer;
         }
 
-//Internal
+        //Internal
 
-        function toString() return this != null ? 'Float32Array [byteLength:${this.byteLength}, length:${this.length}]' : null;
+        function toString() {
+            return this != null ? 'Float32Array [byteLength:${this.byteLength}, length:${this.length}]' : null;
+        }
 
-    inline function get_length() return this.length;
+        inline function get_length() {
+            return this.length;
+        }
 
+        @:noCompletion
+        @:arrayAccess
+        public inline function __get(idx:Int) : Float {
+            return ArrayBufferIO.getFloat32(this.buffer, this.byteOffset+(idx*BYTES_PER_ELEMENT) );
+        }
 
-    @:noCompletion
-    @:arrayAccess
-    public inline function __get(idx:Int) : Float {
-        return ArrayBufferIO.getFloat32(this.buffer, this.byteOffset+(idx*BYTES_PER_ELEMENT) );
+        @:noCompletion
+        @:arrayAccess
+        public inline function __set(idx:Int, val:Float) : Float {
+            return ArrayBufferIO.setFloat32(this.buffer, this.byteOffset+(idx*BYTES_PER_ELEMENT), val);
+        }
     }
-
-    @:noCompletion
-    @:arrayAccess
-    public inline function __set(idx:Int, val:Float) : Float {
-        return ArrayBufferIO.setFloat32(this.buffer, this.byteOffset+(idx*BYTES_PER_ELEMENT), val);
-    }
-
-}
 
 #end //!js
