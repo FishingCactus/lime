f45ffaa8f4f5b0c2e15e962cedb299b6ed4410b9
Ramses Ladlani <ramses.ladlani@fishingcactus.com>
2017-11-07 17:40:27 +0100

    Added option to embed spector

--- lime/_backend/html5/HTML5Window.hx
+++ lime/_backend/html5/HTML5Window.hx
@@ -236,6 +236,12 @@ class HTML5Window {
 			element.addEventListener ("gamepaddisconnected", handleGamepadEvent, true);
 
 		}
+		#if spector
+			untyped __js__("
+				var spector = new SPECTOR.Spector();
+				spector.displayUI();
+				");
+		#end
 	}
 
 
